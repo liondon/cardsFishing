b311859146d49d348a5dfb5a17f8f4e52ed500e6
liondon <liondonaugust810810@gmail.com>
2020-03-30 01:27:04 -0400

    create cardsFishing

---
+++ main.js
@@ -0,0 +1,159 @@
+const GAME_STATE = {
+  FirstCardAwaits: 'FirstCardAwaits',
+  SecondCardAwaits: 'SecondCardAwaits',
+  CardsMatchFailed: 'CardsMatchFailed',
+  CardsMatched: 'CardsMatched',
+  GameFinished: 'GameFinished',
+}
+const Symbols = [
+  'https://image.flaticon.com/icons/svg/105/105223.svg', // 黑桃
+  'https://image.flaticon.com/icons/svg/105/105220.svg', // 愛心
+  'https://image.flaticon.com/icons/svg/105/105212.svg', // 方塊
+  'https://image.flaticon.com/icons/svg/105/105219.svg' // 梅花
+]
+const view = {
+  showGameFinished() {
+    const div = document.createElement('div')
+    div.classList.add('completed')
+    div.innerHTML = `
+      <p>Complete!</p>
+      <p>Score: ${model.score}</p>
+      <p>You've tried: ${model.triedTimes} times</p>
+    `
+    const header = document.querySelector('#header')
+    header.before(div)
+  },
+  appendWrongAnimation(...cards) {
+    cards.map(card => {
+      card.classList.add('wrong')
+      card.addEventListener('animationed', event => {
+        EventTarget.target.classList.removw('wrong'), { once: true }
+      })
+      /* 最後的 {once: true} 是要求在事件執行一次之後，就要卸載這個監聽器。因為同一張卡片可能會被點錯好幾次，每一次都需要動態地掛上一個新的監聽器，並且用完就要卸載。 */
+    })
+  },
+  renderScore(score) {
+    document.querySelector('.score').textContent = `Score: ${score}`
+  },
+  renderTriedTimes(triedTimes) {
+    document.querySelector('.tried').textContent = `You've tried: ${triedTimes} times`
+  },
+  flipCards(...cards) {
+    cards.map(card => {
+      if (card.classList.contains('back')) {
+        card.classList.remove('back')
+        card.innerHTML = this.getCardContent(Number(card.dataset.index))
+        return
+      }
+      card.classList.add('back')
+      card.innerHTML = null
+    })
+  },
+  displayCards(indices) {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = indices.map(index => this.getCardElement(index)).join('');
+  },
+  pairCards(...cards) {
+    cards.map(card => {
+      card.classList.add('paired')
+    })
+  },
+  getCardElement(index) {
+    return `<div class="card back" data-index="${index}"></div>`
+  },
+  getCardContent(index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+    return `
+      <p>${number}</p>
+      <img src="${symbol}">
+      <p>${number}</p>
+    `
+  },
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  }
+}
+const model = {
+  score: 0,
+  triedTimes: 0,
+  revealedCards: [],
+  isRevealedCardsMatched() {
+    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
+  },
+}
+const controller = {
+  currentState: GAME_STATE.FirstCardAwaits,
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52))
+  },
+  dispatchCardAction(card) {
+    if (!card.classList.contains('back')) {
+      return
+    }
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwaits:
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        this.currentState = GAME_STATE.SecondCardAwaits
+        break
+      case GAME_STATE.SecondCardAwaits:
+        view.renderTriedTimes(++model.triedTimes)
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        if (model.isRevealedCardsMatched()) {
+          this.currentState = GAME_STATE.CardsMatched
+          view.renderScore(model.score += 10)
+          view.pairCards(...model.revealedCards)
+          model.revealedCards = []
+          if (model.score === 260) {
+            console.log('showGameFinished')
+            this.currentState = GAME_STATE.GameFinished
+            view.showGameFinished()
+            return
+          }
+          this.currentState = GAME_STATE.FirstCardAwaits
+        } else {
+          this.currentState = GAME_STATE.CardsMatchFailed
+          view.appendWrongAnimation(...model.revealedCards)
+          setTimeout(this.resetCards, 1000)
+        }
+        break
+    }
+    console.log('this.currentState', this.currentState)
+    console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
+  },
+  resetCards() {
+    view.flipCards(...model.revealedCards)
+    model.revealedCards = []
+    controller.currentState = GAME_STATE.FirstCardAwaits
+  }
+}
+const utility = {
+  getRandomNumberArray(count) {
+    const numbers = Array.from(Array(count).keys())
+    for (let index = numbers.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1))
+        ;[numbers[index], numbers[randomIndex]] = [numbers[randomIndex], numbers[index]]  // 此處";"如省略會導致編譯器理解困難, 以為是Math.floor() []
+    }
+    return numbers
+  }
+}
+controller.generateCards()
+/* 可試著以事件委派方式用一個監聽器來處理點擊翻牌 */
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', event => {
+    controller.dispatchCardAction(card)
+  })
+})
